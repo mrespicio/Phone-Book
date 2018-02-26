e59de3faa4b7e4d022a043f73407c3179b1d4923
Megan Respicio <34357105+mrespicio@users.noreply.github.com>
2018-02-25 19:38:22 -0800

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,10 +1,3 @@
-/*
-Assignment#4
-PhoneBook.java
-Megan Respicio
-cssc0935
-*/
-
 import data_structures.*;
 import java.util.Iterator;
 import java.io.*;
@@ -12,16 +5,12 @@ import java.io.*;
 public class PhoneBook <K extends Comparable<K>, V>{
 	private int maxSize;
 	private DictionaryADT<K,V> book;
-    // Constructor.  There is no argument-less constructor, or default size
     public PhoneBook(int maxSize){
     	this.maxSize=maxSize;
     	book = (DictionaryADT<K,V>) new Hashtable<Integer,Integer>(maxSize);
     	//book = (DictionaryADT<K,V>) new BinarySearchTree<Integer,Integer>();
     	//book = (DictionaryADT<K,V>) new BalancedTree<Integer,Integer>();
     }
-    // Reads PhoneBook data from a text file and loads the data into
-    // the PhoneBook.  Data is in the form "key=value" where a phoneNumber
-    // is the key and a name in the format "Last, First" is the value.
     public void load(String filename){
     	String line;
     	try{
@@ -37,33 +26,18 @@ public class PhoneBook <K extends Comparable<K>, V>{
     		System.out.println("Exception" + e);
     	}
     }
-    // Returns the name associated with the given PhoneNumber, if it is
-    // in the PhoneBook, null if it is not.
     public String numberLookup(PhoneNumber number){
     	return (String) book.getValue((K) number);
     }       
-    // Returns the PhoneNumber associated with the given name value.
-    // There may be duplicate values, return the first one found.
-    // Return null if the name is not in the PhoneBook.
     public PhoneNumber nameLookup(String name){
     	return (PhoneNumber) book.getKey((V)name);
     }      
-    // Adds a new PhoneNumber = name pair to the PhoneBook.  All
-    // names should be in the form "Last, First".
-    // Duplicate entries are *not* allowed.  Return true if the
-    // insertion succeeds otherwise false (PhoneBook is full or
-    // the new record is a duplicate).  Does not change the datafile on disk.
     public boolean addEntry(PhoneNumber number, String name){
     	return book.add((K)number, (V)name);
     }    
-    // Deletes the record associated with the PhoneNumber if it is
-    // in the PhoneBook.  Returns true if the number was found and
-    // its record deleted, otherwise false.  Does not change the datafile on disk.
     public boolean deleteEntry(PhoneNumber number){
     	return book.delete((K)number);
     }      
-    // Prints a directory of all PhoneNumbers with their associated
-    // names, in sorted order (ordered by PhoneNumber).
     public void printAll(){
     	Iterator<PhoneNumber> keys = book.keys();
     	Iterator<String> values = book.values();
@@ -73,8 +47,6 @@ public class PhoneBook <K extends Comparable<K>, V>{
     		System.out.println();
     	}
     }     
-    // Prints all records with the given Area Code in ordered
-    // sorted by PhoneNumber.
     public void printByAreaCode(String code){
     	Iterator<K> iter = book.keys();
     	while(iter.hasNext()){
@@ -82,9 +54,7 @@ public class PhoneBook <K extends Comparable<K>, V>{
     		if(code.compareTo(tmp.areaCode)==0)
     			System.out.println(tmp.toString() + ": " + book.getValue((K) tmp));
     	}
-    }  
-    // Prints all of the names in the directory, in sorted order (by name,
-    // not by number).  There may be duplicates as these are the values.       
+    }    
     public void printNames(){
     	Iterator<V> iter = book.values();
     	while(iter.hasNext()) System.out.println(iter.next());
